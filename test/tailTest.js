ffdf60dd9caacda491746e529be0606d57473be9
karlyabut <karleisenyabut@gmail.com>
2019-08-26 18:13:08 +0000

    added mocha and chai to tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,17 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
+// const
 
 
-assertEqual(tail([0, 1, 2, 3]), [1, 2, 3]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
+
+// console.log("tail test", assertEqual(tail([0, 1, 2, 3]), [1, 2, 3]));
